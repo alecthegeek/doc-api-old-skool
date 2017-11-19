b189607e770b791c2ed18c97d102ea6dbfbe81d2
Alec Clews <alec.clews@papercut.com>
2017-11-19 11:07:38 +1100

    Draft Mind Map

---
+++ LCA2018.mm
@@ -0,0 +1,166 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="LCA2018" FOLDED="false" ID="ID_225655949" CREATED="1511043393797" MODIFIED="1511043548346" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Details" POSITION="left" ID="ID_1057516852" CREATED="1511043424492" MODIFIED="1511043678016">
+<edge COLOR="#0000ff"/>
+<node TEXT="Date 22/Jan/18" ID="ID_815341892" CREATED="1511043409027" MODIFIED="1511043430331"/>
+<node TEXT="UTS Broadway Campus, Syndey" ID="ID_1646644440" CREATED="1511043434842" MODIFIED="1511043530695"/>
+<node TEXT="AGENDA" ID="ID_791647676" CREATED="1511043679954" MODIFIED="1511043807390"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ol>
+      <li>
+        <font color="rgb(51, 51, 51)" face="Hack, monospace" size="14px">Who is going to read our documentation and what do they need? </font>
+      </li>
+      <li>
+        <font color="rgb(51, 51, 51)" face="Hack, monospace" size="14px">What types of documents can we create? (API specs, diagrams, text, sample code) </font>
+      </li>
+      <li>
+        <font color="rgb(51, 51, 51)" face="Hack, monospace" size="14px">What tools can we use to create, edit, manage and publish our material)</font>
+      </li>
+      <li>
+        <font color="rgb(51, 51, 51)" face="Hack, monospace" size="14px">How can we bootstrap our docs toolchain?</font>
+      </li>
+      <li>
+        <font color="rgb(51, 51, 51)" face="Hack, monospace" size="14px">Some practical hands on examples and techniques.This will be the main part of the presentation</font>
+      </li>
+    </ol>
+    <p>
+      <font color="rgb(51, 51, 51)" face="Hack, monospace" size="14px">Examples of these tools in use can be seen at https://github.com/alecthegeek/version-control-basics and https://github.com/PaperCutSoftware/howto-xmlrpc-clients -- however more detailed examples will also be provided, including a sample template project.&#160; </font>
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      <font color="rgb(51, 51, 51)" face="Hack, monospace" size="14px">The talk will focus on specific techniques using pandoc, sed, m4 and plantuml to push changes quickly.&#160;</font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="Further Resources" ID="ID_1294076525" CREATED="1511043830918" MODIFIED="1511049873496">
+<node TEXT="https://github.com/alecthegeek/lca2018" ID="ID_1616367367" CREATED="1511044097734" MODIFIED="1511044097734" LINK="https://github.com/alecthegeek/lca2018"/>
+</node>
+<node TEXT="Target Audience" ID="ID_380684225" CREATED="1511044143457" MODIFIED="1511044150864">
+<node ID="ID_187593305" CREATED="1511044151400" MODIFIED="1511044245713"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      API developers who<br/>manage documentation
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
+<node TEXT="Agenda" POSITION="right" ID="ID_935277092" CREATED="1511044251097" MODIFIED="1511044254131">
+<edge COLOR="#00ffff"/>
+<node TEXT="Introduction" ID="ID_1995595482" CREATED="1511044544911" MODIFIED="1511044549769">
+<node TEXT="Who is Alec" ID="ID_203552168" CREATED="1511044557564" MODIFIED="1511044563851"/>
+<node TEXT="What has he done that qualifies him to talk about this" ID="ID_1783483355" CREATED="1511044564586" MODIFIED="1511044586179"/>
+</node>
+<node TEXT="Who will read our docs" ID="ID_39315865" CREATED="1511044254802" MODIFIED="1511044385186">
+<node TEXT="Other developers" ID="ID_527245864" CREATED="1511044457916" MODIFIED="1511044466124"/>
+<node TEXT="Who may not speak English" ID="ID_673705514" CREATED="1511044467069" MODIFIED="1511044482187">
+<node TEXT="Diagrams are important" ID="ID_1428652403" CREATED="1511048772563" MODIFIED="1511048778086"/>
+</node>
+<node TEXT="Will have varying skill levels" ID="ID_211523845" CREATED="1511044483010" MODIFIED="1511044489642"/>
+<node TEXT="Will be using different tools and languages" ID="ID_156950122" CREATED="1511044516642" MODIFIED="1511044537975"/>
+</node>
+<node TEXT="What will they read?" ID="ID_798334146" CREATED="1511044386089" MODIFIED="1511044400350">
+<node TEXT="API docs generated from code (e.g. Javadoc)" ID="ID_784213943" CREATED="1511044592382" MODIFIED="1511044622346"/>
+<node TEXT="Background details on how to use your API" ID="ID_714504924" CREATED="1511044637379" MODIFIED="1511044662417"/>
+<node TEXT="Use cases for specific situations" ID="ID_196788332" CREATED="1511044663110" MODIFIED="1511048283494"/>
+<node TEXT="Answers to previous questions" ID="ID_1911758321" CREATED="1511048917082" MODIFIED="1511048925935"/>
+</node>
+<node TEXT="What tools can we use?" ID="ID_1688376811" CREATED="1511044407895" MODIFIED="1511044413089">
+<node TEXT="Pandoc" ID="ID_1983545106" CREATED="1511048928335" MODIFIED="1511049930831"/>
+<node TEXT="M4 Macro Processor" ID="ID_1097401074" CREATED="1511048931801" MODIFIED="1511048960217"/>
+<node TEXT="sed" ID="ID_324374291" CREATED="1511049918469" MODIFIED="1511049920899"/>
+<node TEXT="Bash sripts" ID="ID_986647041" CREATED="1511048961194" MODIFIED="1511048968789"/>
+<node TEXT="Diagramming tools with CLI" ID="ID_25714948" CREATED="1511048969665" MODIFIED="1511048977430"/>
+<node TEXT="Make (or some other build tool)" ID="ID_235278115" CREATED="1511049127617" MODIFIED="1511049128852"/>
+<node TEXT="A publishing platform e.g GitHub wiki pages" ID="ID_259211037" CREATED="1511049129532" MODIFIED="1511049144189"/>
+<node TEXT="A local review platform" ID="ID_657966232" CREATED="1511049274985" MODIFIED="1511049298104"/>
+<node TEXT="Langauge Specific support" ID="ID_27909276" CREATED="1511049790396" MODIFIED="1511049800194"/>
+</node>
+<node TEXT="Show me how to do it" ID="ID_1038186776" CREATED="1511044446412" MODIFIED="1511044453570">
+<node TEXT="Practical walk through" ID="ID_209493881" CREATED="1511049805271" MODIFIED="1511049819590"/>
+</node>
+</node>
+</node>
+</map>
---
+++ README.md
@@ -0,0 +1,5 @@
+# LCA2018
+
+A presentation on how to use various CLI tools to create API documentation
+
+This repo provides an example as well as being the location for the source material
