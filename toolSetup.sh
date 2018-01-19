72ff85cce1ef2a76bca5b7564f4b452e7fcb7cb3
Alec Clews <alec.clews@papercut.com>
2018-01-20 10:58:08 +1100

    Getting ready for LCA presentation

--- toolSetup.sh
+++ toolSetup.sh
@@ -2,30 +2,58 @@
 
 # Assume we are using Bash
 
+# NOTE: This script assumes it's working on a clean install of Debian
+# If you already have Ruby or Python environments already set up this script
+# may stop them working. Use with disrection.
+
 sudo apt-get update
 sudo apt-get dist-upgrade -y
 
 # Add core doc tools
-sudo apt-get install -y plantuml graphviz make m4 vim-gtk git python3-pip python3-dev python3-setuptools python3-wheel
+sudo apt-get install -y plantuml graphviz make m4 vim-gtk git python3-venv python3-dev python3-wheel python3-setuptools python3-pip python3-pandocfilters python3-psutil python3-wheel
+
+# Actually need to manually install later version of plantuml because Debian version is so out of date
+# This is a dirty hack
+sudo wget https://downloads.sourceforge.net/project/plantuml/1.2018.0/plantuml.1.2018.0.jar -O /usr/share/plantuml/plantuml.jar
+
+# Install a Python virtual env
+
+VENV_HOME=~/py3venv/pandoc
+
+mkdir -p $VENV_HOME
+python3 -m venv $VENV_HOME
+source $VENV_HOME/bin/activate
+
+echo >> ~/.bashrc
+echo "# Added for install of Python venv in $VENV_HOME $(date)" >> ~/.bashrc
+echo source $VENV_HOME/bin/activate >> ~/.bashrc
+
 # Can't install Pandoc from standard repos because it's very out of date
-cd /tmp
-wget https://github.com/jgm/pandoc/releases/download/2.1/pandoc-2.1-1-amd64.deb
-sudo dpkg --install pandoc*.deb
+wget https://github.com/jgm/pandoc/releases/download/2.1/pandoc-2.1-1-amd64.deb -P /tmp
+sudo dpkg --install /tmp/pandoc*.deb
 
 # Want to build PDF files with Pandoc, need latex
 sudo apt-get install -y texlive
 
 # Want x ref for figures in Pandoc
-pip install --upgrade --pre pandoc-figno
+pip install --upgrade wheel
+pip install --upgrade --pre pandoc-fignos
 
+sudo apt-get -y autoremove
+sudo apt-get clean
 
-# GitHub specific set up tools
+# Tools to help publish on GitHub
 
 # Setup Ruby for Gollum
 sudo apt-get install -y rbenv ruby-build  git gcc libssl-dev libreadline-dev zlib1g-dev
 
+sudo apt-get -y autoremove
+sudo apt-get clean
+
 rbenv version
 
+echo >> ~/.bashrc
+echo "# Added for install of Ruby rbenv $(date)" >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc 
 eval "$(rbenv init -)"
 
@@ -40,13 +68,16 @@ gem install gollum rdiscount github-markdown charlock_holmes
 
 # Install gh-md-toc  from https://github.com/ekalinin/github-markdown-toc
 
-mkdir ~/bin
-wget https://raw.githubusercontent.com/ekalinin/github-markdown-toc/master/gh-md-toc -o ~/bin/gh-md-toc
-chmod a+x ~/bin/gh-md-toc
+if ! which gh-md-toc ; then
+  [[ -d ~/bin ]] || mkdir ~/bin
+  wget https://raw.githubusercontent.com/ekalinin/github-markdown-toc/master/gh-md-toc -O ~/bin/gh-md-toc
+  chmod a+x ~/bin/gh-md-toc
 
-if [[ ! -x ~/bin/gh-md-toc ]] ; then
-  # Only works on GNU systems? Beware Mac users
-  printf '\n# Added for install of gh-md-toc %(%F)T\nPATH=~/bin:$PATH\n' >> ~/.bashrc
-  echo "PATH updated. Please restart terminal session"
+  echo >> ~/.bashrc
+  echo "# Added for install of gh-md-toc $(date)" >> ~/.bashrc
+  echo 'PATH=~/bin:$PATH' >> ~/.bashrc
+  echo >> ~/.bashrc
 fi
+
+echo "Pease restart terminal to pick up env changes"
 
